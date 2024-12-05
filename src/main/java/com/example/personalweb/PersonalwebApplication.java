af6ae54082faadc780416996c99834876b6985fc
Fardin <faa35@sfu.ca>
2024-12-05 15:22:35 -0800

    gemini and maps apis

--- src/main/java/com/example/personalweb/PersonalwebApplication.java
+++ src/main/java/com/example/personalweb/PersonalwebApplication.java
@@ -2,6 +2,9 @@ package com.example.personalweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PersonalwebApplication {
@@ -9,5 +12,17 @@ public class PersonalwebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalwebApplication.class, args);
 	}
+	@Bean
+    public WebMvcConfigurer corsFixer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") 
+				.allowedOrigins("http://localhost:3000")
+				.allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+			}
+        };
+    }
 
 }
